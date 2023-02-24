1b65517d787ce3f2b55a696b4d58ab58c6af3d56
Chavozoom <isaquechaves10500@gmail.com>
2023-02-24 00:48:13 -0300

    added the software architecture

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const userRoute = require("./src/routes/user.route")
+
+
 const app = express();
+app.use("/", userRoute)
 
 const port = 4000;
 
-app.get("/", (req, res)=>{
-    res.send("Hello");
-});
 
 app.listen(port, console.log("App is running at port " + port))
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,5 @@
+const ex = (req, res) =>{
+    res.send("hjlkfgsag")
+}
+
+module.exports = {ex}
